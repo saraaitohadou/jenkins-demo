5182009b65039013dd57547b6ac6f943f877b6ea
saraaitohadou <163491957+saraaitohadou@users.noreply.github.com>
2026-04-01 17:08:53 -0700

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 echo ===== PIPELINE JENKINS =====
 echo Build en cours...
 echo Test OK
-echo Deploy simulé
-echo Nouveau build déclenché automatiquement
+echo Deploy simule
+echo Nouveau build declenche automatiquement
